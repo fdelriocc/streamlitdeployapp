31abc410f30b7dfce37d5417635c8bd2679094c7
fdelriocc <fdelriocc@gmail.com>
2023-05-15 17:44:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ fig_sexo = px.histogram(
     y=df_selection["cantidad"], 
     color=df_selection["Movilidad"], 
     barmode="group",
-    title="Cantidad de estudiantes entrantes y salientes por género"
+    title="Cantidad de estudiantes entrantes y salientes por género",
     labels={'x':'Género', 'y':'Cantidad'}
     
     )
